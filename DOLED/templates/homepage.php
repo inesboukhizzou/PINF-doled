56ec96f3b7c1ba941c32acbe797902a072963c84
Ines <119856381+inesboukhizzou@users.noreply.github.com>
2024-02-07 20:51:14 +0100

    Add files via upload

---
+++ DOLED/templates/homepage.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="stylesheet.css">
+    <title>Homepage</title>
+</head>
+<body>
+<div class="background-container">
+    <div class="content">
+        <h1 class="titresH1">
+            Bienvenue Chez Doled :
+            <br>Votre Magasin D'Électronique De Confiance Au Maroc
+        </h1>
+        <p id="sous-titre">Découvrez chez Doled tout ce dont vous rêvez en électronique. <br> Explorez notre catalogue diversifié et profitez d'avantages exclusifs avec notre programme de points de fidélité.</p>
+        <div class="actions">
+            <button class="btn">Voir les Produits</button>
+            <a href="../templates/user.php" class="liensInter">Profitez des Points de Fidélité</a> 
+        </div>
+    </div>
+    <div class="image-homepage">
+        <img id="image-magasin" src="magasin.png" alt="photo du magasin">
+    </div>
+    <div class="products-container">
+        <div class="product">
+            <img src="product1.png" alt="carte arduino">
+            <div class="product-description">
+                <p>Nom produit</p><br>
+                <p>Disponible</p><br>
+                <p>1200dhs</p><br>
+                <button class="btn">Voir les détails</button>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- les div de la page produit -->
+</body>
+</html>
