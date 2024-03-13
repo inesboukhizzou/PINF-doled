d5c9496ef9cf2339d9b0d00fff4293115a69771e
marieddddi <124163576+marieddddi@users.noreply.github.com>
2024-03-13 14:49:05 +0100

    Update supprimer_produit.php

--- DOLEDV4/DOLEDV3/templates/supprimer_produit.php
+++ DOLEDV4/DOLEDV3/templates/supprimer_produit.php
@@ -7,34 +7,62 @@ include_once "libs/maLibSecurisation.php";
 include_once "libs/modele.php";
 
 $listeProduits = listeProduits();
-foreach ($listeProduits as $produit) {
-    echo "<div class='conteneur-produit'>";
-    echo "<div class='produit'>";
-    echo "   <img src='" . $produit["image_prod"] . "' class='photo'>";
-    echo "   <p class='identite'><strong>" . $produit["designation"] . "</strong></p>";
-    echo "   <p class='role'>" . $produit["prix_unitaire"] . "€</p>";
-    echo "  <p class='description'>" . $produit["descriptif_produit"] . "</p>";
-    echo " <p class='marque' >" . $produit["nom_marque"] . "</p>";
-    echo "<input type='button' class='btn3' value='Supprimer' onclick='supprimerProduit(" . $produit["id_produit"] . ")'>";
-
-    echo "</div></div>";
-}
 ?>
+<div id="searchContainer">
+    <input type="text" id="searchProduit" onkeyup="filterProducts()"
+        placeholder="Rechercher par marque, par designation ou par id">
+</div>
+<div id="produitsContainer">
+    <?php
 
+    foreach ($listeProduits as $produit) {
+        echo "<div class='conteneur-produit'>";
+        echo "<div class='produit'>";
+        echo "   <img src='" . $produit["image_prod"] . "' class='photo'>";
+        echo "   <p class='identite'><strong>" . $produit["designation"] . "</strong></p>";
+        echo "   <p class='role'>" . $produit["prix_unitaire"] . "€</p>";
+        echo " <p class='marque' >" . $produit["nom_marque"] . "</p>";
+        echo "<input type='button' class='btn3' value='Supprimer' onclick='supprimerProduit(" . $produit["id_produit"] . ")'>";
+
+        echo "</div></div>";
+    }
+    ?>
+</div>
 <script>
-    function supprimerProduit(idProduit) {
-        // Effectuer une requête AJAX pour supprimer le produit
-        $.ajax({
-            url: 'controleur.php?action=supprimerProduit&id_produit=' + idProduit,
-            method: 'POST',
-            success: function (response) {
-                // Faire quelque chose après la suppression (mettre à jour l'interface utilisateur, etc.)
-                console.log(response);
-                location.reload();
-            },
-            error: function (error) {
-                console.log(error);
+
+
+        function filterProducts() {
+        var input, filter, produits, produit, i, txtValue;
+        input = document.getElementById('searchProduit');
+        filter = input.value.toUpperCase();
+        produits = document.getElementsByClassName('produit');
+
+        for (i = 0; i < produits.length; i++) {
+            produit = produits[i];
+        txtValue = produit.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            produit.style.display = "";
+            } else {
+            produit.style.display = "none";
             }
-        });
+        }
+    }
+
+
+
+        function supprimerProduit(idProduit) {
+            // Effectuer une requête AJAX pour supprimer le produit
+            $.ajax({
+                url: 'controleur.php?action=supprimerProduit&id_produit=' + idProduit,
+                method: 'POST',
+                success: function (response) {
+                    // Faire quelque chose après la suppression (mettre à jour l'interface utilisateur, etc.)
+                    console.log(response);
+                    location.reload();
+                },
+                error: function (error) {
+                    console.log(error);
+                }
+            });
     }
 </script>
